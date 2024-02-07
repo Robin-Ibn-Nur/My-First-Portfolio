9c5a9c99b158d05ee426930c011f898a2001554b
saifsammy <saifsammy653@gmail.com>
2024-02-07 18:49:25 +0600

    banner content update

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -17,31 +17,42 @@ const Banner = () => {
 
 
     return (
-        <div id="home" className="name-banner container mx-auto relative h-96 md:h-[500px] lg:h-screen ">
-            <div className="gradient"></div>
-            <div className="name"
-                data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-delay="50"
-                data-aos-duration="2000"
-                data-aos-easing="ease-in-out"
-                data-aos-mirror="true"
-                data-aos-once="false"
-                data-aos-anchor-placement="top-center"
-            >
-                <main className="text-container flex justify-center items-center">
-                    <svg className="text-stroke" viewBox="0 0 500 100" width="80%" height="100%">
-                        <text className="uppercase" x="15" y="75">
-                            md.robin
-                        </text>
-                    </svg>
-                </main>
-                <a className="reset w-max h-[50px] text-xl flex items-center gap-2" onClick={restartAnimation} href={resume} download>
-                    <span>DOWNLOAD RESUME</span>
-                    <AiOutlineDownload />
-                </a>
+        <div>
+            <div id="home" className="name-banner container mx-auto relative h-96 md:h-[500px] lg:h-screen ">
+                <div className="name"
+                    data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-delay="50"
+                    data-aos-duration="2000"
+                    data-aos-easing="ease-in-out"
+                    data-aos-mirror="true"
+                    data-aos-once="false"
+                    data-aos-anchor-placement="top-center"
+                >
+                    {/* <main className="text-container flex justify-center items-center">
+                        <svg className="text-stroke" viewBox="0 0 500 100" width="80%" height="100%">
+                            <text className="uppercase" x="15" y="75">
+                                md.robin
+                            </text>
+                        </svg>
+                    </main> */}
+                    <div className="keyboard">
+                        <span className="key">M</span>
+                        <span className="key">D</span>
+                        <span className="key">.</span>
+                        <span className="key">R</span>
+                        <span className="key">O</span>
+                        <span className="key">B</span>
+                        <span className="key">I</span>
+                        <span className="key">N</span>
+                    </div>
+                    <a className="reset w-max h-[50px] text-xl flex items-center gap-2" onClick={restartAnimation} href={resume} download>
+                        <span>DOWNLOAD RESUME</span>
+                        <AiOutlineDownload />
+                    </a>
 
 
+                </div>
             </div>
         </div>
 
